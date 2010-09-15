6f0a3a0605c4f0e96775065eaf8f37719280f306
Tj Holowaychuk <tj@vision-media.ca>
2010-09-15 13:17:11 -0700

    Added .createClient() test

--- test/ns3.test.js
+++ test/ns3.test.js
@@ -8,5 +8,18 @@ var ns3 = require('ns3');
 module.exports = {
   'test .version': function(assert){
     assert.match(ns3.version, /^\d+\.\d+\.\d+$/);
+  },
+  
+  'test .createClient()': function(assert){
+    var client = ns3.createClient({
+        key: 'foobar'
+      , secret: 'baz'
+      , bucket: 'misc'
+    });
+    
+    assert.equal('foobar', client.key);
+    assert.equal('baz', client.secret);
+    assert.equal('misc', client.bucket);
+    assert.equal('s3.amazonaws.com', client.host);
   }
 };
