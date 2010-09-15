ba9fb91ff470f1ddbce88885e54ef7a30d34ba4b
Tj Holowaychuk <tj@vision-media.ca>
2010-09-15 14:59:32 -0700

    Added PUT / GET tests

--- test/ns3.test.js
+++ test/ns3.test.js
@@ -8,6 +8,7 @@ var ns3 = require('ns3')
 
 try {
   var auth = JSON.parse(fs.readFileSync('auth', 'ascii'));
+  var client = ns3.createClient(auth);
 } catch (err) {
   console.error('`make test` requires ./auth to contain a JSON string with');
   console.error('`key, secret, and bucket in order to run tests.');
@@ -58,7 +59,39 @@ module.exports = {
     assert.equal('s3.amazonaws.com', client.host);
   },
   
-  'test PUT': function(assert){
-    
+  'test PUT': function(assert, done){
+    var path = __dirname + '/fixtures/user.json'
+      , n = 0;
+    fs.stat(path, function(err, stat){
+      if (err) throw err;
+      fs.readFile(path, function(err, buf){
+        if (err) throw err;
+        var req = client.put('/test/user.json', {
+          'Content-Length': stat.size,
+          'Content-Type': 'application/json'
+        });
+        req.on('response', function(res){
+          switch (++n) {
+            case 1:
+              assert.equal(100, res.statusCode);
+              break;
+            case 2:
+              assert.equal(200, res.statusCode);
+              done();
+              break;
+          }
+        });
+        req.end(buf);
+      })
+    });
+  },
+  
+  'test GET': function(assert, done){
+    client.get('/test/user.json').on('response', function(res){
+      assert.equal(200, res.statusCode);
+      assert.equal('application/json', res.headers['content-type'])
+      assert.equal(13, res.headers['content-length'])
+      done();
+    }).end();
   }
 };
