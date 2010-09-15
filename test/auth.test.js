a983ee5bfebb7087c591487975554b0f0a26d324
Tj Holowaychuk <tj@vision-media.ca>
2010-09-15 11:32:12 -0700

    Added auth.sign() test

--- test/auth.test.js
+++ test/auth.test.js
@@ -24,5 +24,17 @@ module.exports = {
     ].join('\n');
     
     assert.equal(expected, str);
+  },
+  
+  'test .sign()': function(assert){
+    var str = auth.sign({
+        verb: 'PUT'
+      , md5: '09c68b914d66457508f6ad727d860d5b'
+      , contentType: 'text/plain'
+      , resource: '/learnboost'
+      , date: new Date('may 25 1987')
+    });
+
+    assert.equal('9huBjjAJN00uCvd14ofvHu6mhGE=', str);
   }
 };
