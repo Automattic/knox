eb0b60b3b3e0b113c8557fb3d932b74c73cfc890
Alvin Lumowa <wtflol@gmail.com>
2013-03-28 23:54:13 -0700

    Fixed overwriting bucket name with undefined.

--- test/initClients.js
+++ test/initClients.js
@@ -12,7 +12,7 @@ module.exports = function(){
     client = knox.createClient(auth);
 
     var auth2 = utils.merge({}, auth);
-    auth2.bucket = auth2.bucket2;
+    auth2.bucket = auth.bucket;
     client2 = knox.createClient(auth2);
 
     var authUsWest2 = utils.merge({}, auth);
