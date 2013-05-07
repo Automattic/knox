51608c4711ac0bc7a65b06ecfe2f0e58cf838eaf
Domenic Denicola <domenic@domenicdenicola.com>
2013-05-06 22:27:39 -0400

    More auth signing tests; closes #6.
    This contains all of the tests from #6 except for the problematic one with multiple headers. We'll wait for someone to actually want that before adding it.
    
    There is a minor functionality improvement: x-amz-date headers are now ignored, instead of breaking the request, and if you use the auth library directly, you can pass strings instead of `Date` instances for the `date` option.

--- test/auth.test.js
+++ test/auth.test.js
@@ -40,6 +40,107 @@ describe('knox.auth', function () {
     });
   });
 
+  describe('.authorization() [from the Amazon docs]', function () {
+    // http://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAuthentication.html#RESTAuthenticationExamples
+
+    specify('Example Object GET', function () {
+      var actual = auth.authorization({
+          verb: 'GET'
+        , key: 'AKIAIOSFODNN7EXAMPLE'
+        , secret: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+        , resource: auth.canonicalizeResource('/johnsmith/photos/puppy.jpg')
+        , date: 'Tue, 27 Mar 2007 19:36:42 +0000'
+      });
+
+      assert.equal(actual, 'AWS AKIAIOSFODNN7EXAMPLE:bWq2s1WEIj+Ydj0vQ697zp+IXMU=');
+    });
+
+    specify('Example Object PUT', function () {
+      var actual = auth.authorization({
+          verb: 'PUT'
+        , key: 'AKIAIOSFODNN7EXAMPLE'
+        , secret: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+        , resource: auth.canonicalizeResource('/johnsmith/photos/puppy.jpg')
+        , contentType: 'image/jpeg'
+        , date: 'Tue, 27 Mar 2007 21:15:45 +0000'
+      });
+
+      assert.equal(actual, 'AWS AKIAIOSFODNN7EXAMPLE:MyyxeRY7whkBe+bq8fHCL/2kKUg=');
+    });
+
+    specify('Example List', function () {
+      var actual = auth.authorization({
+          verb: 'GET'
+        , key: 'AKIAIOSFODNN7EXAMPLE'
+        , secret: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+        , resource: auth.canonicalizeResource('/johnsmith/')
+        , date: 'Tue, 27 Mar 2007 19:42:41 +0000'
+      });
+
+      assert.equal(actual, 'AWS AKIAIOSFODNN7EXAMPLE:htDYFYduRNen8P9ZfE/s9SuKy0U=');
+    });
+
+    specify('Example Fetch', function () {
+      var actual = auth.authorization({
+          verb: 'GET'
+        , key: 'AKIAIOSFODNN7EXAMPLE'
+        , secret: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+        , resource: auth.canonicalizeResource('/johnsmith/?acl')
+        , date: 'Tue, 27 Mar 2007 19:44:46 +0000'
+      });
+
+      assert.equal(actual, 'AWS AKIAIOSFODNN7EXAMPLE:c2WLPFtWHVgbEmeEG93a4cG37dM=');
+    });
+
+    specify('Example Delete', function () {
+      // This is modified from the docs: knox does not allow setting the date
+      // through x-amz-date, so we test that the x-amz-date is ignored and the
+      // date is instead used.
+      var actual = auth.authorization({
+          verb: 'DELETE'
+        , key: 'AKIAIOSFODNN7EXAMPLE'
+        , secret: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+        , resource: auth.canonicalizeResource('/johnsmith/photos/puppy.jpg')
+        , amazonHeaders: auth.canonicalizeHeaders({
+            'x-amz-date': 'Tue, 27 Mar 2007 21:20:27 +0000'
+          })
+        , date: 'Tue, 27 Mar 2007 21:20:26 +0000'
+      });
+
+      assert.equal(actual, 'AWS AKIAIOSFODNN7EXAMPLE:lx3byBScXR6KzyMaifNkardMwNk=');
+    });
+
+    specify.skip('Example Upload', function () {
+      // Knox doesn't support multiple values for a single header; see
+      // discussion at https://github.com/LearnBoost/knox/pull/6. Elegant pull
+      // requests to implement this feature welcome!
+    });
+
+    specify('Example List All My Buckets', function () {
+      var actual = auth.authorization({
+          verb: 'GET'
+        , key: 'AKIAIOSFODNN7EXAMPLE'
+        , secret: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+        , resource: auth.canonicalizeResource('/')
+        , date: 'Wed, 28 Mar 2007 01:29:59 +0000'
+      });
+
+      assert.equal(actual, 'AWS AKIAIOSFODNN7EXAMPLE:qGdzdERIC03wnaRNKh6OqZehG9s=');
+    });
+
+    specify('Example Unicode Keys', function () {
+      var actual = auth.authorization({
+          verb: 'GET'
+        , key: 'AKIAIOSFODNN7EXAMPLE'
+        , secret: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+        , resource: auth.canonicalizeResource('/dictionary/fran%C3%A7ais/pr%c3%a9f%c3%a8re')
+        , date: 'Wed, 28 Mar 2007 01:49:49 +0000'
+      });
+
+      assert.equal(actual, 'AWS AKIAIOSFODNN7EXAMPLE:DNEZGsoieTZ92F3bUfSPQcbGmlM=');
+    });
+  });
+
   describe('.canonicalizeHeaders()', function () {
     specify('with no Amazon headers', function () {
       assert.equal(auth.canonicalizeHeaders({}), '');
@@ -58,7 +159,6 @@ describe('knox.auth', function () {
           'x-amz-acl:private'
         , 'x-amz-copy-source:/bucket/object'
         , 'x-amz-copy-source-if-match:etagvalue'
-        , 'x-amz-date:some date'
       ].join('\n');
 
       assert.equal(actual, expected);
